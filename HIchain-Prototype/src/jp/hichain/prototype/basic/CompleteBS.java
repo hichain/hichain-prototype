4bcf2b499d35cfa9e35a3c5e221f5f29de0edd14
Tokiwa <tkw.4402@gmail.com>
2016-12-13 14:03:01 +0900

    Reflesh SignData

--- HIchain-Prototype/src/jp/hichain/prototype/basic/CompleteBS.java
+++ HIchain-Prototype/src/jp/hichain/prototype/basic/CompleteBS.java
@@ -1,5 +1,7 @@
 package jp.hichain.prototype.basic;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
 /**
@@ -31,6 +33,11 @@ public class CompleteBS extends BoardSign {
 	 */
 	public CompleteBS (int _player, char _ch, int _dir) {
 		super(_player, _ch, _dir);
+	}
+
+	public CompleteBS(int _player, char _ch, int _dir, int [] _nums, int _ps, BufferedImage _image) {
+		super(_player, _ch, _dir);
+		putSign(_player, _ch, _dir, _nums, _ps, _image);
 	}
 
 	/**
@@ -68,24 +75,83 @@ public class CompleteBS extends BoardSign {
 
 	/**
 	 * 文字を置く
-	 * プレイヤー番号、文字、文字の向きを代入し、それらよりSN・PS・SIを代入 (座標は置換しない)
+	 * プレイヤー番号、文字、文字の向き、SN、PS、SIを代入 (座標は置換しない)
 	 * @param _player プレイヤー番号
 	 * @param _ch 文字
 	 * @param _dir 文字の向き
+	 * @param _nums 4方向のSN
+	 * @param _ps PS
+	 * @param _image SI
 	 */
-	@Override
-	public void putSign(int _player, char _ch, int _dir) {
-		super.putSign(_player, _ch, _dir);
-		nums = SignData.getNum(_ch, _dir);
-		ps = SignData.getPS(_ch, _dir);
-		image = SignData.getImage(_player, _ch, _dir);
+	public void putSign(int _player, char _ch, int _dir, int [] _nums, int _ps, BufferedImage _image) {
+		putSign(_player, _ch, _dir);
+		nums = _nums;
+		ps = _ps;
+		image = _image;
+		rotate(_dir);
 	}
 
 	/**
 	 * 文字を置く (BS指定)
-	 * @param _bs BS
+	 * @param _bs CompleteBS
 	 */
-	void putSign(BoardSign _bs) {
-		putSign( _bs.getPlayer(), _bs.getChar(), _bs.getDir());
+	public void putSign(CompleteBS _bs) {
+		putSign( _bs.getPlayer(), _bs.getChar(), _bs.getDir(), _bs.getNums(), _bs.getPS(), _bs.getImage() );
+	}
+
+	/**
+	 * 文字を回転させる
+	 * @param _dir 回転数(反時計回り)
+	 */
+	public void rotate(int _dir) {
+		rotatePS(_dir);
+		rotateSN(_dir);
+		rotateSI(_dir);
+	}
+
+	/**
+	 * SNを回転する
+	 * @param _dir 回転数(反時計回り)
+	 */
+	private void rotateSN(int _dir) {
+		for (int i = 0; i < _dir; ++i) {
+			int tmp = nums[3];
+			for (int j = nums.length-1; j > 0; --j) {
+				nums[j] = nums[j-1];
+			}
+			nums[0] = tmp;
+		}
+	}
+
+	/**
+	 * PSを回転する
+	 * @param _dir 回転数(反時計回り)
+	 */
+	private void rotatePS(int _dir) {
+		_dir %= 4;
+		if (_dir <= 0) return;
+
+		int newPs = 0;
+
+		for (int i = 0; i < 4; i++) {
+			newPs ^= (ps & (0x800 >>> 2*i)) << 4+i;    //0000 1000 0000 0000
+			newPs ^= (ps & (0x400 >>> 2*i)) >>> 7-i;   //0000 0100 0000 0000
+			newPs ^= (ps & (0x8000 >>> i)) >>> 11-3*i; //1000 0000 0000 0000
+			newPs ^= (ps & (0x8 >>> i)) << 2+3*i;      //0000 0000 0000 1000
+		}
+
+		rotatePS(--_dir);
+	}
+
+	/**
+	 * SIを回転する
+	 * @param _dir 回転数(反時計回り)
+	 */
+	private void rotateSI (int _dir) {
+		_dir %= 4;
+
+		AffineTransform tx = AffineTransform.getQuadrantRotateInstance(_dir);
+		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		image = op.filter(image, null);
 	}
 }
--- HIchain-Prototype/src/jp/hichain/prototype/basic/SignData.java
+++ HIchain-Prototype/src/jp/hichain/prototype/basic/SignData.java
@@ -11,12 +11,13 @@ public class SignData {
 
 	/**
 	 * 指定の文字のデータが存在するか返す
-	 * @param _ch 文字
+	 * @param _player プレイヤー番号
+	 * @param _ch 文字のchar
 	 * @return true/false
 	 */
-	static public boolean contains(char _ch) {
-		for (CompleteBS sign : signs) {
-			if (sign.getChar() == _ch) {
+	static public boolean contains(int _player, char _ch) {
+		for (CompleteBS bs : signs) {
+			if (bs.getPlayer() == _player && bs.getChar() == _ch) {
 				return true;
 			}
 		}
@@ -36,94 +37,79 @@ public class SignData {
 	 * @return 文字のSet
 	 */
 	static public Set <Character> getSignSet() {
-		Set <Character> chars = new HashSet <Character> ();
-		for (CompleteBS sign : signs) {
-			chars.add(sign.getChar());
+		Set <Character> signSet = new HashSet<Character>();
+		for (CompleteBS bs : signs) {
+			signSet.add(bs.getChar());
 		}
-		return chars;
+		return signSet;
 	}
 
 	/**
 	 * 文字を追加する
 	 * @param _bs BS
 	 */
-	static public void add(int _player, char _ch, BufferedImage _img, int [] num, int ps) {
-		CompleteBS bs = new CompleteBS(_player, _ch, 0);
+	static public void add(int _player, char _ch, int [] _nums, int _ps, BufferedImage _image) {
+		CompleteBS bs = new CompleteBS(_player, _ch, 0, _nums, _ps, _image);
+		add(bs);
+	}
 
-		signs.add();
+	public static void add(CompleteBS _bs) {
+		signs.add(_bs);
 	}
 
 	/**
-	 * 文字を取得する
+	 * BSを取得する
 	 * @param _ch 文字のchar
 	 * @return BS
 	 */
-	static public CompleteBS get(char _ch) {
-		for (CompleteBS sign : signs) {
-			if (sign.getChar() == _ch) {
-				return sign;
+	static public CompleteBS get(int _player, char _ch) {
+		for (CompleteBS bs: signs) {
+			if (bs.getPlayer() == _player && bs.getChar() == _ch) {
+				return bs;
 			}
 		}
-		return get(' ');
+		return signs.get(' ');
 	}
 
-  /**
-   * 回転したSNを返す
-   * @param _nums 4方向のSN
-   * @param _dir 文字の向き(回転数・反時計回り)
-   * @return SN
-   */
-  static private int [] rotateNum(int [] _nums, int _dir) {
-    for (int i = 0; i < _dir; ++i) {
-      int tmp = _nums[3];
-      for (int j = _nums.length-1; j > 0; --j) {
-        _nums[j] = _nums[j-1];
-      }
-      _nums[0] = tmp;
-    }
+	/**
+	 * SNを取得する
+	 * @param _ch 文字のchar
+	 * @return SN
+	 */
+	static public int [] getSN(char _ch) {
+		for (CompleteBS bs : signs) {
+			if (bs.getChar() == _ch) {
+				return bs.getNums();
+			}
+		}
+		return signs.get(' ').getNums();
+	}
 
-    return _nums;
-  }
+	/**
+	 * PSを取得する
+	 * @param _ch 文字のchar
+	 * @return PS
+	 */
+	static public int getPS(char _ch) {
+		for (CompleteBS bs : signs) {
+			if (bs.getChar() == _ch) {
+				return bs.getPS();
+			}
+		}
+		return signs.get(' ').getPS();
+	}
 
-  /**
-   * 回転したPSを返す
-   * @param _ps PS
-   * @param _dir 文字の向き
-   * @return PS
-   */
-  static private int rotatePS(int _ps, int _dir) {
-    _dir %= 4;
-    if (_dir <= 0) return _ps;
-
-    int newPs = 0;
-
-    for (int i = 0; i < 4; i++) {
-      newPs ^= (_ps & (0x800 >>> 2*i)) << 4+i;    //0000 1000 0000 0000
-      newPs ^= (_ps & (0x400 >>> 2*i)) >>> 7-i;   //0000 0100 0000 0000
-      newPs ^= (_ps & (0x8000 >>> i)) >>> 11-3*i; //1000 0000 0000 0000
-      newPs ^= (_ps & (0x8 >>> i)) << 2+3*i;      //0000 0000 0000 1000
-    }
-
-    return rotatePS(newPs, --_dir);
-  }
-
-  /**
-   * 回転したSIを返す
-   * @param _img SI
-   * @param _dir 文字の向き
-   * @return SI
-   */
-  static private BufferedImage rotateImage(BufferedImage _img, int _dir) {
-    _dir %= 4;
-    if (_dir <= 0) return _img;
-
-    int r = _img.width;
-    color [] pixels = _img.pixels.clone();
-    for (int i = 0; i < _img.pixels.length; i++) {
-      int j = (r-1-i/r) + r*(i%r);
-      _img.pixels[i] = pixels[j];
-    }
-
-    return rotateImage(_img, --_dir);
-  }
+	/**
+	 * SIを取得する
+	 * @param _ch 文字のchar
+	 * @return SI
+	 */
+	static public BufferedImage getSI(int _player, char _ch) {
+		for (CompleteBS bs : signs) {
+			if (bs.getChar() == _ch && bs.getPlayer() == _player) {
+				return bs.getImage();
+			}
+		}
+		return signs.get(' ').getImage();
+	}
 }
