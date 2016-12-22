03875cc91e1fa32b4424d1b6847c8a4376b0beaf
TokiwaTools <dn.3402@gmail.com>
2016-12-23 03:50:04 +0900

    create enum AroundDir

---
+++ HIchain-Prototype/src/jp/hichain/prototype/concept/AroundDir.java
@@ -0,0 +1,60 @@
+package jp.hichain.prototype.concept;
+
+/**
+ * 文字の周り
+ * 8方向
+ * @author NT
+ *
+ */
+public enum AroundDir {
+	NORTH {
+		@Override
+		public AroundDir getOpposite() {
+			return SOUTH;
+		}
+	},
+	NORTHEAST {
+		@Override
+		public AroundDir getOpposite() {
+			return SOUTHWEST;
+		}
+	},
+	EAST {
+		@Override
+		public AroundDir getOpposite() {
+			return WEST;
+		}
+	},
+	SOUTHEAST {
+		@Override
+		public AroundDir getOpposite() {
+			return NORTHWEST;
+		}
+	},
+	SOUTH {
+		@Override
+		public AroundDir getOpposite() {
+			return NORTH;
+		}
+	},
+	SOUTHWEST {
+		@Override
+		public AroundDir getOpposite() {
+			return NORTHEAST;
+		}
+	},
+	WEST {
+		@Override
+		public AroundDir getOpposite() {
+			return EAST;
+		}
+	},
+	NORTHWEST {
+		@Override
+		public AroundDir getOpposite() {
+			return SOUTHEAST;
+		}
+	};
+
+	public abstract AroundDir getOpposite();
+}
