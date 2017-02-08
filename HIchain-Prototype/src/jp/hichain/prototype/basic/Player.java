19a9dc94ab710a779beff5669fce5109d9bf39ad
TokiwaTools <dn.3402@gmail.com>
2017-02-09 02:39:00 +0900

    :sparkles: create Player class

---
+++ HIchain-Prototype/src/jp/hichain/prototype/basic/Player.java
@@ -0,0 +1,16 @@
+package jp.hichain.prototype.basic;
+
+import java.util.List;
+
+import jp.hichain.prototype.concept.CardColor;
+
+/**
+ * プレイヤー
+ * @author NT
+ *
+ */
+public class Player {
+	String name;
+	CardColor cardColor;
+	List <Move> moves;
+}
