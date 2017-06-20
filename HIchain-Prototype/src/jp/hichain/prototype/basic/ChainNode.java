5c607ca88fffec54e2d48cd0015db36a8d4ebd49
Tokiwa <tkw.4402@gmail.com>
2017-06-20 18:38:11 +0900

    [wip] solving loop bug: ChainNode
    introduce active flag

--- HIchain-Prototype/src/jp/hichain/prototype/basic/ChainNode.java
+++ HIchain-Prototype/src/jp/hichain/prototype/basic/ChainNode.java
@@ -7,18 +7,25 @@ import java.util.*;
 public class ChainNode {
 	private final Square thisSquare;
 	private EnumMap<Relation, Set<ChainNode>> relationMap;
+	private EnumMap<Relation, Boolean> activeMap;
 	private boolean valid = true;
 	private boolean mature = false;
-	private final boolean asterisk;
 
 	public ChainNode(Square _thisSq) {
+		this(_thisSq, false);
+	}
+
+	public ChainNode(Square _thisSq, boolean asterisk) {
 		thisSquare = _thisSq;
 		relationMap = new EnumMap<Relation, Set<ChainNode>>(Relation.class) {{
 			put(Relation.PARENT, new HashSet<>());
 			put(Relation.CHILD, new HashSet<>());
 		}};
+		activeMap = new EnumMap<Relation, Boolean>(Relation.class) {{
+			put(Relation.PARENT, !asterisk);
+			put(Relation.CHILD, !asterisk);
+		}};
 		valid = !thisSquare.hasPluralChains();
-		asterisk = thisSquare.getSign().getSC().get() == '*';
 	}
 
 	public enum Relation {
@@ -52,6 +59,7 @@ public class ChainNode {
 	}
 
 	public boolean isEdgeOf(Edge edge) {
+		if (!isActive( edge.getRelation() )) return true;
 		return relationMap.get( edge.getRelation() ).size() == 0;
 	}
 
@@ -69,11 +77,20 @@ public class ChainNode {
 		mature = _mature;
 	}
 
+	public boolean isActive(Relation relation) {
+		return activeMap.get(relation);
+	}
+
+	public void setActive(Relation relation) {
+		activeMap.put(relation, true);
+	}
+
 	public Square getSquare() {
 		return thisSquare;
 	}
 
 	public Set<ChainNode> get(Relation relation) {
+		if (!isActive(relation)) return new HashSet<>();
 		return relationMap.get(relation);
 	}
 
@@ -89,10 +106,6 @@ public class ChainNode {
 	public void setValidAll(boolean _valid) {
 		setValidAll(Relation.PARENT, _valid);
 		setValidAll(Relation.CHILD, _valid);
-	}
-
-	public boolean isAsterisk() {
-		return asterisk;
 	}
 
 	@Override
